79e733dfdf121ec9f1d4e3dfc40b1db4847d426a
thaynsworth <thomas.haynsworth@gmail.com>
2016-01-23 13:18:50 -0500

    updated margins and email formatting

--- keystone.js
+++ keystone.js
@@ -53,7 +53,7 @@ keystone.set('routes', require('./routes'));
 // default email templates, you may remove them if you're using your own.
 
 keystone.set('email locals', {
-	logo_src: '/images/logo-email.gif',
+	logo_src: '/images/jbr-logo.png',
 	logo_width: 194,
 	logo_height: 76,
 	theme: {
