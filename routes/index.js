0fe5dd478c52e32a736a8e010ff1f87dcef0712f
thaynsworth <thomas.haynsworth@gmail.com>
2016-01-30 17:29:36 -0500

    changed routing settings

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ var routes = {
 exports = module.exports = function(app) {
 
 	// Views
-	app.all('/', routes.views.contact);
+	app.get('/', routes.views.contact);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
