82479558b7f0122f4ac97f5af83a2db2f63ac792
thaynsworth <thomas.haynsworth@gmail.com>
2016-01-31 20:24:55 -0500

    updated routing

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -36,29 +36,29 @@ Enquiry.schema.post('save', function() {
 });
 
 Enquiry.schema.methods.sendNotificationEmail = function(callback) {
-	
+
 	if ('function' !== typeof callback) {
 		callback = function() {};
 	}
-	
+
 	var enquiry = this;
-	
+
 	keystone.list('User').model.find().where('isAdmin', true).exec(function(err, admins) {
-		
+
 		if (err) return callback(err);
-		
+
 		new keystone.Email('enquiry-notification').send({
 			to: admins,
 			from: {
 				name: 'JBR Holdings',
-				email: 'contact@jbr-holdings.com'
+				email: 'info@jbrholdings.com'
 			},
 			subject: 'New Enquiry for JBR Holdings',
 			enquiry: enquiry
 		}, callback);
-		
+
 	});
-	
+
 };
 
 Enquiry.defaultSort = '-createdAt';
