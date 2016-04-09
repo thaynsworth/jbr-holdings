af05dd1933006be9afea2cd568bbc800e000574d
thaynsworth <thomas.haynsworth@gmail.com>
2016-04-09 15:35:01 -0400

    added thank you page for contact form

--- routes/middleware.js
+++ routes/middleware.js
@@ -27,7 +27,8 @@ exports.initLocals = function(req, res, next) {
 		{ label: 'Home',		key: 'home',		href: '/' },
 		{ label: 'Blog',		key: 'blog',		href: '/blog' },
 		{ label: 'Gallery',		key: 'gallery',		href: '/gallery' },
-		{ label: 'Contact',		key: 'contact',		href: '/contact' }
+		{ label: 'Contact',		key: 'contact',		href: '/contact' },
+		{ label: 'ThankYou',		key: 'thankyou',		href: '/thankyou' }
 	];
 
 	locals.user = req.user;
